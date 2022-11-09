e285c8e404444a27b9389a9958539e5904398864
bin.zhang <739367755@qq.com>
2022-11-09 16:02:34 +0800

    feat: 只在dev的阶段进行提示

--- src/components/NotificationJumpVs/index.jsx
+++ src/components/NotificationJumpVs/index.jsx
@@ -2,7 +2,22 @@ import React, { useEffect } from 'react'
 import { notification } from 'antd';
 const NotificationJumpVs = (props) => {
 
-  useEffect(() => {
+
+  const isDev = () => {
+    // 不能是线上地址 可以接受线上的域名
+    // 不能是测试环境，不包含test
+    let devFlags = ['localhost', '127.0.0.1']
+    let isOK = devFlags.some((item,index,arr)=>{
+      return location.href.includes(item)
+  })
+    if (isOK) {
+      return true
+    }
+
+    return false
+  };
+
+  const reminderUser = () => {
     const args = {
       message: '如何点击页面跳转到vscode？',
       description:
@@ -10,6 +25,14 @@ const NotificationJumpVs = (props) => {
       duration: 0,
     };
     notification.open(args);
+  };
+
+  useEffect(() => {
+
+    if (isDev) {
+      reminderUser()
+    }
+
   }, [])
 
   return (
